1dbd377ff3c1620874df49738b55fe0a2302df74
Jason Crockett <JesusisLord33333@outlook.com>
2022-03-12 15:50:38 +0000

    Initial commit

---
+++ lib/changelly.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "changelly/version"
+
+module Changelly
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/changelly/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Changelly
+  VERSION = "0.1.0"
+end
